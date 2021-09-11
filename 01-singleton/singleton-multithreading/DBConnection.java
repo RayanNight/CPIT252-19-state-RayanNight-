ffa4ac3e4abdd08151e4893797d5903893f12314
Khalid Alharbi <kalharbi@users.noreply.github.com>
2021-09-11 21:31:08 +0300

    thread safe 3rd attempt. The double-checked locking Singelton implementation

--- 01-singleton/singleton-multithreading/DBConnection.java
+++ 01-singleton/singleton-multithreading/DBConnection.java
@@ -1,19 +1,32 @@
 package singletonMultiThreadingExample;
 
-public class DBConnection{
-  // Create an instance here early on and the JVM will guarantee that the 
-  // instance will be created before any thread accessed it. 
-  private static DBConnection uniqueInstance = new DBConnection();;
+/**
+ * Double-checked locking singelton implementation.
+ */
+
+public class DBConnection {
+  // The volatile keyword guarantees visibility but NOT atomicity. It ensures that
+  // multiple threads handle the variable correctly becuase it's always read from
+  // main
+  // memory and not the thread's cache.
+  private static volatile DBConnection uniqueInstance;
 
   // properties or instance variables here
   private int portNumber;
+
   private String hostName;
-  // TODO: Add getters and setters
+  // TODO: Add tters and setters
 
-  private DBConnection(){}
+  private DBConnection() {}
 
-  public static  DBConnection getInstance(){
+  public static DBConnection getInstance(){
+    if (uniqueInstance == null) { // if there's no instance, enter the synchronized block
+      synchronized (DBConnection.class) {
+        if (uniqueInstance == null) {
+          uniqueInstance = new DBConnection();
+        }
+      }
+    }
     return uniqueInstance;
   }
 }
-
