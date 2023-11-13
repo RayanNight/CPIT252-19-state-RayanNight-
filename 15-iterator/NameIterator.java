35af68a8822e8960542f2fcd779da4649c12a7bd
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-11-13 14:32:56 +0300

    use arrays and arraylist

--- 15-iterator/NameIterator.java
+++ 15-iterator/NameIterator.java
@@ -1,17 +1,26 @@
 package iterator;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class NameIterator implements Iterator {
 
-  String names[];
+  String[] names;
+  //List<String> names;
   int index;
 
-  public NameIterator(String[] newName){
-    this.names = newName;
+  public NameIterator(String[] newNames) {
+    this.names = newNames;
+    // this.names = new ArrayList<String>();
+    // for (String name : newNames) {
+    //   this.names.add(name);
+    // }
   }
 
   @Override
   public boolean hasNext() {
     if(index <names.length){
+    // if (index < names.size()) {
       return true;
     }
     return false;
@@ -19,13 +28,10 @@ public class NameIterator implements Iterator {
 
   @Override
   public Object next() {
-    if(this.hasNext()){
-      return names[index++]; 
+    if (this.hasNext()) {
+      return names[index++];
+      // return names.get(index++);
     }
     return null;
   }
 }
-
-
-
-
